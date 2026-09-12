33626a55c6e11ca2187d22ef95aa81baf87c3fa9
m3ysalem <m.3ysalem@gmail.com>
2026-09-12 08:56:44 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          DEFAULT: '#9E1B1B',
-          dark: '#801414',
-          light: '#FDF2F2',
-        },
-        ink: {
-          900: '#111315',
-          800: '#14171A',
-          700: '#26292D',
-        },
-        surface: '#f4f6f9',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
